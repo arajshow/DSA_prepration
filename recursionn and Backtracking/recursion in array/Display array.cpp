7b0fa81339704966fb32a1b8585cc1bab088cf94
Raj <20bec061@nith.ac.in>
2021-11-16 12:58:01 +0530

    first

---
+++ recursionn and Backtracking/recursion in array/Display array.cpp
@@ -0,0 +1,21 @@
+/*
+1. You are given a number n, representing the size of array a.
+2. You are given n numbers, representing elements of array a.
+3. You are required to print the elements of array from beginning to end each in a separate line.
+4. For the above purpose complete the body of displayArr function. Don't change the signature.
+
+Note -> The online judge can't force you to write the function recursively but that is what
+the spirit of question is. Write recursive and not iterative logic. The purpose of
+the question is to aid learning recursion and not test you.
+
+Input Format
+A number n
+n1
+n2
+.. n number of elements
+
+Output Format
+n1
+n2
+.. n elements
+*/
